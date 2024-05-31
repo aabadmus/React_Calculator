24e986dd02120dcc5d6d6fbeb317e3857bc55621
Agareovo1 <ogheneovoagare@gmail.com>
2024-05-31 15:18:08 +0100

    addition const

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 
 
 const Calculator = () => {
- 
+  //application of useState hook, where result is the initial state and setResult is the function that updates the result
+  const [result, setResult] = useState('');
+  const [scientificOperation, setScientificOperation] = useState('');
 
   //this function handles the handlebuttonclick button by use of effect
   const handleButtonClick = (e) => {
