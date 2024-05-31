2d56f31bcd440ec1d8b00417bdf122f22db46114
Agareovo1 <ogheneovoagare@gmail.com>
2024-05-31 15:04:16 +0100

    buffer creation

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,12 +1,49 @@
 import { useState } from "react";
 import './App.css';
 
-function App() {
+
+const Calculator = () => {
+ 
+
+
+
+ 
+
+
+ 
+
+ 
+
+
+
+
+
+
+
   return (
-    <div className="App">
-     
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="border border-black sm:w-96 md:w-120 bg-black p-6 rounded-md shadow-md">
+        <div className="">
+          {/*the buffer begins here */}
+          <form> 
+            <input
+              type="text"
+              value={result}
+              className="buffer text-cyan-50 font-semibold sm:text-2xl md:text-3xl"
+            />
+          </form>
+          {/*the buffer ends here */}
+          <div className="gridContainer">
+           
+           
+            
+            
+            
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Calculator;
