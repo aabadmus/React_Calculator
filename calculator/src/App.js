0cbbf0bd20fc1aab6e1ef8f99a6e6cf981c451b5
Agareovo1 <ogheneovoagare@gmail.com>
2024-05-31 15:06:21 +0100

    numbers buttons introduction

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -38,7 +38,57 @@ const Calculator = () => {
            
             
             
-            
+            <button name="7" onClick={handleButtonClick} id="numberButton">
+              7
+            </button>
+            <button name="8" onClick={handleButtonClick} id="numberButton">
+              8
+            </button>
+            <button name="9" onClick={handleButtonClick} id="numberButton">
+              9
+            </button>
+            <button className="" name="*" onClick={handleButtonClick} id="operatorButton">
+              &times;
+            </button>
+            <button name="4" onClick={handleButtonClick} id="numberButton">
+              4
+            </button>
+            <button name="5" onClick={handleButtonClick} id="numberButton">
+              5
+            </button>
+            <button name="6" onClick={handleButtonClick} id="numberButton">
+              6
+            </button>
+            <button className="" name="-" onClick={handleButtonClick} id="operatorButton">
+              &ndash;
+            </button>
+            <button name="1" onClick={handleButtonClick} id="numberButton">
+              1
+            </button>
+            <button name="2" onClick={handleButtonClick} id="numberButton">
+              2
+            </button>
+            <button name="3" onClick={handleButtonClick} id="numberButton">
+              3
+            </button>
+            <button className="" name="+" onClick={handleButtonClick} id="operatorButton">
+              +
+            </button>
+            <button name="0" onClick={handleButtonClick} id="numberButton">
+              0
+            </button>
+            <button name="." onClick={handleButtonClick} id="operatorButton">
+              .
+            </button>
+            <button className="" onClick={() => addBracket('(')} id="operatorButton">
+              (
+            </button>
+            <button className="" onClick={() => addBracket(')')} id="operatorButton">
+              )
+            </button>
+            <button className="" onClick={performCalculation} id="resultButton">
+              =
+            </button>
           </div>
         </div>
       </div>
