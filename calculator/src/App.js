f2ddcd2aa2b2a8726f0f23e42b6d7d81dbbf0bb3
Agareovo1 <ogheneovoagare@gmail.com>
2024-05-31 15:16:33 +0100

    const clear result

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -7,7 +7,10 @@ const Calculator = () => {
 
 
 
- 
+  const clearResult = () => {
+    setResult('');
+    setScientificOperation('');
+  };
 
   const removeLastCharacter = () => {
     setResult(result.slice(0, -1));
